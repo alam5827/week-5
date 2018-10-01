6c8e14dc83ed9498b7668cc26c92bde87f4e70e9
alam5827 <42847203+alam5827@users.noreply.github.com>
2018-10-01 14:57:20 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
+});
